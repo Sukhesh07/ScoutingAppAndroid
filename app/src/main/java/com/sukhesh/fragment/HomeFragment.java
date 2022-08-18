3fe95c1d61a3206fc10bc24aaca4949bcdcbe049
sukheshCoding <sukheshkumar.saravanan@gmail.com>
2022-08-18 19:34:23 -0400

    Initial commit

---
+++ app/src/main/java/com/sukhesh/fragment/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.sukhesh.fragment;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ScrollView;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
