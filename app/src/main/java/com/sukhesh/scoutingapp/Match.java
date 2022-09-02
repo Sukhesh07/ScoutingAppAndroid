9dfd7ca82a09350b524eb42099ec9aaadba7f4ee
feds201 <feds201@gmail.com>
2022-09-02 14:05:36 -0400

    Match.storeMatch to use fields in class instead of hardcoded statements

--- app/src/main/java/com/sukhesh/scoutingapp/Match.java
+++ app/src/main/java/com/sukhesh/scoutingapp/Match.java
@@ -3,7 +3,6 @@ package com.sukhesh.scoutingapp;
 import android.content.SharedPreferences;
 
 import java.lang.reflect.*;
-import java.util.ArrayList;
 
 
 public class Match {
@@ -26,24 +25,61 @@ public class Match {
     }
 
     public static Match MatchFromSharedPreferences(SharedPreferences sp) {
-        String matchType = sp.getString("TYPE", "Qualification");
-        int matchNumber = sp.getInt("MATCH_NUMBER", 1);
-        int teamNumber = sp.getInt("TEAM_NUMBER", 201);
-        String teamName = sp.getString("TEAM_NAME", "The FEDS");
-        String robotAllianceInfo = sp.getString("ROBOT_ALLIANCE_INFO", "Red 1");
+        String matchType = sp.getString(Match.variableNameToConstName("matchType"), "Qualification");
+        int matchNumber = sp.getInt(Match.variableNameToConstName("matchNumber"), 1);
+        int teamNumber = sp.getInt(Match.variableNameToConstName("typeNumber"), 201);
+        String teamName = sp.getString(Match.variableNameToConstName("teamName"), "The FEDS");
+        String robotAllianceInfo = sp.getString(Match.variableNameToConstName("robotAllianceInfo"), "Red 1");
         return new Match(matchType, matchNumber, teamNumber, teamName, robotAllianceInfo);
     }
 
     public void storeMatch(SharedPreferences sp) {
         SharedPreferences.Editor editor = sp.edit();
-        editor.putString("TYPE", this.matchType);
-        editor.putInt("MATCH_NUMBER", this.matchNumber);
-        editor.putInt("TEAM_NUMBER", this.teamNumber);
-        editor.putString("TEAM_NAME", this.teamName);
-        editor.putString("ROBOT_ALLIANCE_INFO", this.robotAllianceInfo);
+        Field[] fieldList = this.getClass().getDeclaredFields();
+        for(Field fld: fieldList) {
+            String typeName = fld.getType().getTypeName();
+            String constName = Match.variableNameToConstName(fld.getName());
+            switch (typeName) {
+                case "java.lang.String":
+                    try {
+                        editor.putString(constName, fld.get(this).toString());
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "int":
+                    try {
+                        editor.putInt(constName, Integer.parseInt(fld.get(this).toString()));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                case "double":
+                case "float":
+                    try {
+                        editor.putFloat(constName, Float.parseFloat(fld.get(this).toString()));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
         editor.apply();
     }
 
+    private static String variableNameToConstName(String name) {
+        // essentially, if the variable name was matchType, this returns MATCH_TYPE
+        String constName = "";
+        for(int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (Character.isLowerCase(c)) {
+                constName += Character.toUpperCase(c);
+            } else if (Character.isUpperCase(c)) {
+                constName += "_";
+                constName += c;
+            }
+        }
+        return constName;
+    }
 
     // TODO: extract this to class citrusCiruitStyleMessage and pass in fields in initializer
     public String citrusCircuitStyleMessage() {
