a66e922c0ec8c76a93b9b5725a447639b9df47f7
sukheshCoding <sukheshkumar.saravanan@gmail.com>
2022-09-01 20:10:50 -0400

    Implemented QR Generator

--- app/src/main/java/com/sukhesh/scoutingapp/Dashboard.java
+++ app/src/main/java/com/sukhesh/scoutingapp/Dashboard.java
@@ -2,6 +2,7 @@ package com.sukhesh.scoutingapp;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,7 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class Dashboard extends Fragment {
 
@@ -24,6 +32,25 @@ public class Dashboard extends Fragment {
 
         text.setText(currentMatch.matchName());
 
+        //Qr generator, added implementation in build.gradle
+        //https://github.com/zxing/zxing
+        Button btGenerate = rootView.findViewById(R.id.bt_generate);
+        ImageView ivOutput = rootView.findViewById(R.id.iv_output);
+        String str = "Hello";
+            btGenerate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    MultiFormatWriter writer = new MultiFormatWriter();
+                    try {
+                        BitMatrix matrix = writer.encode(str, BarcodeFormat.QR_CODE, 500, 500);
+                        BarcodeEncoder encoder = new BarcodeEncoder();
+                        Bitmap bitmap = encoder.createBitmap(matrix);
+                        ivOutput.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         return rootView;
     }
 }
